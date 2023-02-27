d3804df06b6751f771dab92b1d3f87f36141265a
Joana Moreira <joanamoreira989@gmail.com>
2023-02-27 14:49:28 +0000

    add custom footer

--- mosaic-site/src/components/footer/Footer.tsx
+++ mosaic-site/src/components/footer/Footer.tsx
@@ -1,12 +1,6 @@
 import React from "react";
 import classnames from "classnames";
-import {
-  HelpLinks,
-  LinkButton,
-  H6,
-  P2,
-} from "@jpmorganchase/mosaic-components";
-import type { LinkIconProps } from "@jpmorganchase/mosaic-components";
+import { Link } from "@salt-ds/core";
 import styles from "./Footer.module.css";
 
 type FooterLinkItem = {
@@ -16,17 +10,6 @@ type FooterLinkItem = {
 
 interface FooterProps {
   className?: string;
-  description?: string;
-  helpLinks?: {
-    stackoverflowLabel: string;
-    stackoverflowUrl: string;
-    symphonyLabel: string;
-    symphonyUrl: string;
-  };
-  href?: string;
-  label?: string;
-  iconProps?: LinkIconProps;
-  title?: string;
   links: FooterLinkItem[];
   copyright?: string;
 }
@@ -35,36 +18,14 @@ const defaultCopyright = `© ${new Date().getFullYear()} JPMorgan Chase & Co. Al
 
 export const Footer: React.FC<FooterProps> = ({
   className,
-  description,
-  helpLinks,
-  title,
-  href,
-  label,
   links,
   copyright = defaultCopyright,
 }) => (
   <footer className={classnames(styles.root, className)}>
-    <div className={styles.content}>
-      {title && <H6 className={styles.title}>{title}</H6>}
-      {description && <P2 className={styles.description}> {description}</P2>}
-      {label && href && (
-        <div className={styles.button}>
-          <LinkButton href={href}>{label}</LinkButton>
-        </div>
-      )}
-    </div>
-
-    {helpLinks && (
-      <div className={styles.links}>
-        {helpLinks ? <HelpLinks subTitle="Need Help?" {...helpLinks} /> : null}
-      </div>
-    )}
-
     <div className={styles.links}>
       {links.map(({ label, to }) => (
         <div key={label}>
-          <p>{label}</p>
-          <p>{to}</p>
+          <Link href={to}>{label}</Link>
         </div>
       ))}
     </div>
