17022f79bc02da15d65c3d16e1d4fdfa04dbd42a
Joana Moreira <joanamoreira989@gmail.com>
2023-02-27 14:42:03 +0000

    add custom footer

--- mosaic-site/src/components/footer/Footer.tsx
+++ mosaic-site/src/components/footer/Footer.tsx
@@ -9,6 +9,11 @@ import {
 import type { LinkIconProps } from "@jpmorganchase/mosaic-components";
 import styles from "./Footer.module.css";
 
+type FooterLinkItem = {
+  label?: string;
+  to?: string;
+};
+
 interface FooterProps {
   className?: string;
   description?: string;
@@ -22,7 +27,11 @@ interface FooterProps {
   label?: string;
   iconProps?: LinkIconProps;
   title?: string;
+  links: FooterLinkItem[];
+  copyright?: string;
 }
+
+const defaultCopyright = `© ${new Date().getFullYear()} JPMorgan Chase & Co. All rights reserved.`;
 
 export const Footer: React.FC<FooterProps> = ({
   className,
@@ -31,10 +40,12 @@ export const Footer: React.FC<FooterProps> = ({
   title,
   href,
   label,
+  links,
+  copyright = defaultCopyright,
 }) => (
   <footer className={classnames(styles.root, className)}>
     <div className={styles.content}>
-      {title && <H6 className={styles.title}>Test {title}</H6>}
+      {title && <H6 className={styles.title}>{title}</H6>}
       {description && <P2 className={styles.description}> {description}</P2>}
       {label && href && (
         <div className={styles.button}>
@@ -48,5 +59,18 @@ export const Footer: React.FC<FooterProps> = ({
         {helpLinks ? <HelpLinks subTitle="Need Help?" {...helpLinks} /> : null}
       </div>
     )}
+
+    <div className={styles.links}>
+      {links.map(({ label, to }) => (
+        <div key={label}>
+          <p>{label}</p>
+          <p>{to}</p>
+        </div>
+      ))}
+    </div>
+
+    <div className={styles.copyright}>
+      <p>{copyright}</p>
+    </div>
   </footer>
 );
